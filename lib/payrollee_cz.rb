91ee10655b202e66a84aeb0cb7045370b072a73e
ladislavlisy <ladislav.lisy@seznam.cz>
2013-07-21 11:28:29 +0200

    export results

--- lib/payrollee_cz.rb
+++ lib/payrollee_cz.rb
@@ -5,13 +5,10 @@ require "payrollee_cz/core/payroll_period"
 require "payrollee_cz/core_items/tag_refer"
 require "payrollee_cz/core_items/code_name_refer"
 require "payrollee_cz/core_items/payroll_tag"
-require "payrollee_cz/core_items/payroll_name"
 require "payrollee_cz/core_items/payroll_concept"
 require "payrollee_cz/core_items/payroll_result"
 require 'payrollee_cz/concepts/unknown_concept'
 require "payrollee_cz/core/pay_concept_gateway"
 require 'payrollee_cz/pay_tags/unknown_tag'
 require "payrollee_cz/core/pay_tag_gateway"
-require 'payrollee_cz/pay_tag_names/unknown_name'
-require "payrollee_cz/core/pay_name_gateway"
 require "payrollee_cz/core/payroll_process"
